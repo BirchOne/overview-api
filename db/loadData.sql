748ad9bf8839fd3a4305df4b9b218ead5cbdeb39
Jessica Yang <jessicaawhy@gmail.com>
2021-11-18 16:48:22 -0800

    Create data loader file

---
+++ db/loadData.sql
@@ -0,0 +1,8 @@
+\c overview;
+
+\COPY products FROM './db/data/product.csv' WITH DELIMITER ',' CSV HEADER;
+\COPY features FROM './db/data/features.csv' WITH DELIMITER ',' CSV HEADER;
+\COPY styles FROM './db/data/styles.csv' WITH DELIMITER ',' NULL AS 'null' CSV HEADER;
+\COPY photos FROM './db/data/photosfixed.csv' WITH DELIMITER ',' NULL AS 'null' CSV HEADER;
+\COPY skus FROM './db/data/skus.csv' WITH DELIMITER ',' NULL AS 'null' CSV HEADER;
+\COPY related FROM './db/data/related.csv' WITH DELIMITER ',' NULL AS 'null' CSV HEADER;
