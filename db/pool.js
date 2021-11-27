4b773dd2994677fd1b56943da29b762cf6cffb04
Jessica Yang <jessicaawhy@gmail.com>
2021-11-27 14:44:58 -0800

    Update connection env variables for postgres pools

--- db/pool.js
+++ db/pool.js
@@ -2,6 +2,8 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   max: 20,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
   allowExitOnIdle: true,
